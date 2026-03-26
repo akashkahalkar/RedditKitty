e8e69bdd28efd32042786f6b24fbb3360cdc7638
Akash Kahalkar <akash.kahalkar.08@gmail.com>
2026-03-27 00:07:43 +0530

    switched the tab items

--- RedditKitty/views/ContentView.swift
+++ RedditKitty/views/ContentView.swift
@@ -3,16 +3,14 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         TabView {
-            FetchProfileView()
-                .tabItem {
-                    Label("Fetch", systemImage: "arrow.down.circle")
-                }
-
             SavedProfilesView()
                 .tabItem {
                     Label("Profiles", systemImage: "person.2")
                 }
-
+            FetchProfileView()
+                .tabItem {
+                    Label("Fetch", systemImage: "arrow.down.circle")
+                }
             SettingsView()
                 .tabItem {
                     Label("Settings", systemImage: "gearshape")
