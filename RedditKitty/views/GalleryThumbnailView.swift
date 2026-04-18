6752289dffd191e85bd329d1eb73f868567fb9ad
Akash Kahalkar <akash.kahalkar.08@gmail.com>
2026-04-18 18:12:57 +0530

    removed commented code

--- RedditKitty/views/GalleryThumbnailView.swift
+++ RedditKitty/views/GalleryThumbnailView.swift
@@ -16,37 +16,5 @@ struct GalleryThumbnailView: View {
                 .fill(.red.opacity(0.15))
                 .aspectRatio(aspectRatio, contentMode: .fill)
         }
-
-//        CachedRemoteImage(url: imageURL, thumbnailURL: thumbnailURL) { image in
-//            let image = Image(uiImage: image)
-//            image
-//                .resizable()
-//                .frame(maxWidth: .infinity)
-//                .aspectRatio(aspectRatio, contentMode: .fill)
-//                .clipped()
-//                .transition(.opacity)
-//        } placeholder: { thumbnail in
-//            if let thumbnail {
-//                let thumbnailImage = Image(uiImage: thumbnail)
-//                thumbnailImage
-//                    .resizable()
-//                    .frame(maxWidth: .infinity)
-//                    .aspectRatio(aspectRatio, contentMode: .fill)
-//                    .clipped()
-//                    .blur(radius: 1)
-//            } else {
-//                Rectangle()
-//                    .fill(.gray.opacity(0.2))
-//                    .aspectRatio(aspectRatio, contentMode: .fill)
-//                    .overlay {
-//                        ProgressView()
-//                    }
-//            }
-//
-//        } failure: {
-//            Rectangle()
-//                .fill(.red.opacity(0.15))
-//                .aspectRatio(aspectRatio, contentMode: .fill)
-//        }
     }
 }
